97464a523752b06376237edbb9ff23e3c05e5df3
mohebifar <netfars@gmail.com>
2015-03-08 09:05:24 +0330

    Initial Commit

---
+++ Gruntfile.js
@@ -0,0 +1,78 @@
+var grunt = require('grunt');
+require('load-grunt-tasks')(grunt);
+
+grunt.initConfig({
+  '6to5': {
+    options: {
+      sourceMap: true,
+      modules: 'ignore'
+    },
+    dist: {
+      files: [{
+        src: '.tmp/es6/concat.js',
+        dest: '.tmp/es5/concat.js'
+      }]
+    }
+  },
+  concat: {
+    options: {
+      sourceMap: true,
+      separator: '\n'
+    },
+    dist: {
+      src: [
+        'src/intro.js',
+        '.tmp/es5/concat.js',
+        'src/outro.js'
+      ],
+      dest: 'dist/mol3d.js'
+    },
+    es6: {
+      src: ['src/**/*.js', '!src/intro.js', '!src/outro.js'],
+      dest: '.tmp/es6/concat.js'
+    }
+  },
+  uglify: {
+    options: {
+      sourceMap: true
+    },
+    build: {
+      files: {
+        'dist/mol3d.min.js': ['dist/mol3d.js']
+      }
+    }
+  },
+  watch: {
+    options: {
+      spawn: false
+    },
+    livereload: {
+      files: ['src/**/*.js', 'examples/**/*'],
+      options: {
+        livereload: true
+      }
+    },
+    scripts: {
+      files: ['src/**/*.js'],
+      tasks: ['default']
+    },
+    examples: {
+      files: ['examples/**/*']
+    }
+  },
+  jshint: {
+    options: {
+      jshintrc: '.jshintrc'
+    },
+    allFiles: [
+      'src/**/*.js', '!src/intro.js', '!src/outro.js'
+    ]
+  },
+  clean: {
+    tmp: ['.tmp']
+  }
+});
+
+grunt.registerTask('default', ['clean', 'concat:es6', '6to5', 'concat:dist']);
+
+grunt.registerTask('build', ['default', 'uglify']);
